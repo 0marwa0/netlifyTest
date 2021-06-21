603d27299b6e475ade2cd4b41f511eba0ecd5530
marwa <marwajawad19@gmail.com>
2021-06-21 16:27:10 +0300

    adding connect db

--- src/api.js
+++ src/api.js
@@ -5,7 +5,18 @@ const mongooseDB = require("mongoose");
  
  const app = express();
  const router = express.Router();
- 
+ const connectDB = async()=>{
+  try {
+    await mongoose.connect("mongodb+srv://marwa:marwa@cluster0.9vo0e.mongodb.net/test", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  });
+  } catch (error) {
+    console.log("error when connect to the datebase ",error)
+  }}
+  connectDB()
  router.get("/", (req, res) => {
    res.json({
      hello: "hi!"
