23d4ddb8e18c0f3e65bc7c45097cb4e9359932c5
marwa <marwajawad19@gmail.com>
2021-06-21 17:23:43 +0300

    add the rest of get request

--- src/api.js
+++ src/api.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const mongooseDB = require("mongoose");
-
- 
  const app = express();
  const router = express.Router();
  const connectDB = async()=>{
@@ -19,8 +17,21 @@ const mongooseDB = require("mongoose");
   connectDB()
 
 
+  app.get("/getall", (req, res) => {
+    try {Store.find()
+  .then((data) => {
+  res.send(data);
+    }) .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
+    });
+    } catch {
+      console.lgo("somthing worng happend")
+      }
+  });
 
-  app.get("/getSesstion", (req, res) => {console.log(req.params.id)
+  app.get("/getsesstion", (req, res) => {console.log(req.params.id)
     try {Store.findById(req.params.id)
   .then((data) => {
   res.send(data);
@@ -29,20 +40,17 @@ const mongooseDB = require("mongoose");
         error: err,
       });
     });
-  
-      
     } catch {
       console.lgo("somthing worng happend")
-    
       }
   });
- router.get("/", (req, res) => {
-   res.json({
-     hello: "hi!"
-   });
- });
+//  router.get("/", (req, res) => {
+//    res.json({
+//      hello: "hi!"
+//    });
+//  });
  
- app.use(`/.netlify/functions/api`, router);
+//  app.use(`/.netlify/functions/api`, router);
  
  module.exports = app;
  module.exports.handler = serverless(app);
